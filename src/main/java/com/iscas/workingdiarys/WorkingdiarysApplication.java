effe209b1459e3e365653a2d83ea7144484610e2
daiyongbing <1379358788@qq.com>
2019-01-18 10:29:29 +0800

    初始化SpringBoot项目

---
+++ src/main/java/com/iscas/workingdiarys/WorkingdiarysApplication.java
@@ -0,0 +1,14 @@
+package com.iscas.workingdiarys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingdiarysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkingdiarysApplication.class, args);
+    }
+
+}
+
