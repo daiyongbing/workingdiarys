800733f25fbe0fdc139970c63539a47b7f631974
daiyongbing <37990048+daiyongbing@users.noreply.github.com>
2019-01-18 11:18:47 +0800

    新建UserController类

---
+++ src/main/java/com/iscas/workingdiarys/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.iscas.workingdiarys.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description:    UserController
+ * @Author:         daiyongbing
+ * @CreateDate:     2019/1/18
+ * @Version:        1.0
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+}
