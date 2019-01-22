fe396057a0b3ffb7bdb7a43e150b5dbb862833cd
daiyongbing <37990048+daiyongbing@users.noreply.github.com>
2019-01-22 09:34:24 +0800

    新增Base64工具类

---
+++ src/main/java/com/iscas/workingdiarys/util/encrypt/Base64Util.java
@@ -0,0 +1,56 @@
+package com.iscas.workingdiarys.util.encrypt;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Base64;
+
+/**
+ * @Description:    Base64工具类
+ * @Author:         daiyongbing
+ * @CreateDate:     2019/1/21
+ * @Version:        1.0
+ */
+public class Base64Util {
+    public static String encode2String(String source){
+        return Base64.getEncoder().encodeToString(source.getBytes());
+    }
+
+    public static String encode2String(byte[] bytes){
+        return Base64.getEncoder().encodeToString(bytes);
+    }
+
+    public static byte[] encode2Bytes(String source){
+        return Base64.getEncoder().encode(source.getBytes());
+    }
+    public static byte[] encode2Bytes(byte[] bytes){
+        return Base64.getEncoder().encode(bytes);
+    }
+
+
+    public static byte[] decode2Bytes(String source){
+        return Base64.getDecoder().decode(source);
+    }
+
+    public static byte[] decode2Bytes(byte[] bytes){
+        return Base64.getDecoder().decode(bytes);
+    }
+
+    public static String decode2String(String str) {
+        String decodedStr = "";
+        try {
+            decodedStr =  new String(Base64.getDecoder().decode(str), "utf8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return decodedStr;
+    }
+
+    public static String decode2String(byte[] bytes) {
+        String decodedStr = "";
+        try {
+            decodedStr =  new String(Base64.getDecoder().decode(bytes), "utf8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return decodedStr;
+    }
+}
