1a9cccae03ca958ff2c661ca4f9fbf47311723e8
daiyongbing <37990048+daiyongbing@users.noreply.github.com>
2019-01-24 09:34:00 +0800

    新增PropertiesService服务类,用于获取properties配置文件属性值

---
+++ src/main/java/com/iscas/workingdiarys/exception/RuntimeException.java
@@ -0,0 +1,10 @@
+package com.iscas.workingdiarys.exception;
+
+/**
+ * @Description:    运行时异常
+ * @Author:         daiyongbing
+ * @CreateDate:     2019/1/22
+ * @Version:        1.0
+ */
+public class RuntimeException {
+}
---
+++ src/main/java/com/iscas/workingdiarys/service/PropertiesService.java
@@ -0,0 +1,13 @@
+package com.iscas.workingdiarys.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description:    获取application.properties属性值
+ * @Author:         daiyongbing
+ * @CreateDate:     2019/1/22
+ * @Version:        1.0
+ */
+@Service
+public class PropertiesService {
+}
